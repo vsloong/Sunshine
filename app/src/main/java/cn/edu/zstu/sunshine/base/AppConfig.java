d2c0d411f3865c64578d05064dfdaada14f75313
CooLoongWu <742420210@qq.com>
2017-08-25 11:23:05 +0800

    添加greenDAO，并简单封装

---
+++ app/src/main/java/cn/edu/zstu/sunshine/base/AppConfig.java
@@ -0,0 +1,15 @@
+package cn.edu.zstu.sunshine.base;
+
+/**
+ * App的配置类
+ * Created by CooLoongWu on 2017-8-25 11:06.
+ */
+
+public class AppConfig {
+
+    private static final String DB_NAME = "sunshine";
+
+    public static String getDBName() {
+        return DB_NAME;
+    }
+}
---
+++ app/src/main/java/cn/edu/zstu/sunshine/utils/DaoUtil.java
@@ -0,0 +1,36 @@
+package cn.edu.zstu.sunshine.utils;
+
+import cn.edu.zstu.sunshine.base.AppConfig;
+import cn.edu.zstu.sunshine.base.BaseApplication;
+import cn.edu.zstu.sunshine.greendao.DaoMaster;
+import cn.edu.zstu.sunshine.greendao.DaoSession;
+
+/**
+ * GreenDao的工具类
+ * Created by CooLoongWu on 2017-8-25 10:24.
+ */
+
+public class DaoUtil {
+
+    private static DaoUtil daoUtil;
+    private static DaoMaster daoMaster;
+    private static DaoSession daoSession;
+    private static final String DB_NAME = AppConfig.getDBName();
+
+    public static DaoUtil getInstance() {
+        if (daoUtil == null) {
+            daoUtil = new DaoUtil();
+        }
+        return daoUtil;
+    }
+
+    private DaoUtil() {
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(BaseApplication.getAppContext(), DB_NAME);
+        daoMaster = new DaoMaster(helper.getWritableDb());
+        daoSession = daoMaster.newSession();
+    }
+
+    public DaoSession getSession() {
+        return daoSession;
+    }
+}
