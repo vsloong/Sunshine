3051cec857d2c9eb338f0a9ce41a1d10c7831197
CooLoongWu <742420210@qq.com>
2017-09-01 11:22:09 +0800

    限制添加用户的数量

--- app/src/main/java/cn/edu/zstu/sunshine/tools/user/AddUserViewModel.java
+++ app/src/main/java/cn/edu/zstu/sunshine/tools/user/AddUserViewModel.java
@@ -3,6 +3,8 @@ package cn.edu.zstu.sunshine.tools.user;
 import android.content.Context;
 import android.view.View;
 
+import com.orhanobut.logger.Logger;
+
 import cn.edu.zstu.sunshine.R;
 import cn.edu.zstu.sunshine.base.AppConfig;
 import cn.edu.zstu.sunshine.databinding.ActivityAddStudentBinding;
@@ -18,12 +20,23 @@ import cn.edu.zstu.sunshine.utils.ToastUtil;
 
 public class AddUserViewModel {
 
+    public static final String INTENT_ADD_USER_TYPE = "INTENT_ADD_USER_TYPE";
+
     private Context context;
     private ActivityAddStudentBinding binding;
+
+    private boolean isThisClose = false;
 
     AddUserViewModel(Context context, ActivityAddStudentBinding binding) {
         this.context = context;
         this.binding = binding;
+
+        getIntent();
+    }
+
+    private void getIntent() {
+        isThisClose = ((AddUserActivity) context).getIntent().getBooleanExtra(AddUserViewModel.INTENT_ADD_USER_TYPE, false);
+        Logger.e("Intent：" + isThisClose);
     }
 
     /**
@@ -43,6 +56,11 @@ public class AddUserViewModel {
         User user = userDao.queryBuilder().where(UserDao.Properties.StudentId.eq(id)).build().unique();
         if (user == null) {
             userDao.insert(new User(id, name));
+            if (isThisClose) {
+                ToastUtil.showShortToast(R.string.toast_user_add_success);
+                ((AddUserActivity) context).finish();
+                return false;
+            }
             return true;
         } else {
             ToastUtil.showShortToast(R.string.toast_user_exist);
--- app/src/main/java/cn/edu/zstu/sunshine/tools/user/UserViewModel.java
+++ app/src/main/java/cn/edu/zstu/sunshine/tools/user/UserViewModel.java
@@ -1,14 +1,21 @@
 package cn.edu.zstu.sunshine.tools.user;
 
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.ObservableField;
 import android.view.View;
 
+import com.orhanobut.logger.Logger;
+
+import java.util.List;
+
+import cn.edu.zstu.sunshine.R;
 import cn.edu.zstu.sunshine.base.AppConfig;
 import cn.edu.zstu.sunshine.databinding.ActivityUserBinding;
 import cn.edu.zstu.sunshine.entity.User;
 import cn.edu.zstu.sunshine.greendao.UserDao;
 import cn.edu.zstu.sunshine.utils.DaoUtil;
+import cn.edu.zstu.sunshine.utils.ToastUtil;
 
 /**
  * 用户中心的ViewModel
@@ -29,13 +36,39 @@ public class UserViewModel {
 
 
         UserDao userDao = DaoUtil.getInstance().getSession().getUserDao();
-        User user = userDao.queryBuilder().where(UserDao.Properties.StudentId.eq(AppConfig.getDefaultStudentId())).build().unique();
-        userId.set(user.getStudentId());
-        userName.set(user.getStudentName());
+
+        List<User> users = userDao.queryBuilder().build().list();
+
+        for (User user : users) {
+            if (user.getStudentId().equals(AppConfig.getDefaultStudentId())) {
+                userId.set(user.getStudentId());
+                userName.set(user.getStudentName());
+            }
+
+            Logger.e("用户:" + user.getStudentId() + "；姓名：" + user.getStudentName());
+        }
 
     }
 
     public void onBtnBackClick(View view) {
         ((UserActivity) context).finish();
+    }
+
+    public void onBtnAddUserClick(View view) {
+        UserDao userDao = DaoUtil.getInstance().getSession().getUserDao();
+
+        //如果用户列表为空，那么说明这是用户第一次添加用户，把该用户的ID设为默认ID
+        if (userDao.queryBuilder().build().list().size() > 2) {
+            ToastUtil.showShortToast(R.string.toast_user_full);
+        } else {
+            startNewActivity(AddUserActivity.class);
+        }
+
+    }
+
+    private void startNewActivity(Class cla) {
+        Intent intent = new Intent(context, cla);
+        intent.putExtra(AddUserViewModel.INTENT_ADD_USER_TYPE, true);
+        context.startActivity(intent);
     }
 }
