a29e96638a81ee7fbec0ec07f1e6fe49ef3737b3
CooLoongWu <742420210@qq.com>
2017-09-12 17:19:46 +0800

    主页透明状态栏加图片“沉浸”

--- app/src/main/java/cn/edu/zstu/sunshine/tools/main/MainActivity.java
+++ app/src/main/java/cn/edu/zstu/sunshine/tools/main/MainActivity.java
@@ -1,7 +1,12 @@
 package cn.edu.zstu.sunshine.tools.main;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 
 import com.orhanobut.logger.Logger;
 
@@ -27,6 +32,14 @@ public class MainActivity extends BaseActivity {
         MainActivityViewModel viewModel = new MainActivityViewModel(this, binding);
         binding.setViewModel(viewModel);
 
+        //设置透明状态栏
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 
 //        new DialogUtil(this, R.layout.dialog_base).show();
 
@@ -63,4 +76,19 @@ public class MainActivity extends BaseActivity {
                 });
     }
 
+
+    /**
+     * 监听返回按键【当点击返回键时执行Home键效果】
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            startActivity(intent);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
