adc44481f7b940e12845d49ee86bc2c34e6b2748
CooLoongWu <742420210@qq.com>
2017-11-09 10:20:19 +0800

    由于RecyclerView布局重用机制导致单选错乱，所以每一个布局都必须单独去进行判断和更改内容信息

--- app/src/main/java/cn/edu/zstu/sunshine/tools/timetable/TimetableAddActivity.java
+++ app/src/main/java/cn/edu/zstu/sunshine/tools/timetable/TimetableAddActivity.java
@@ -6,18 +6,14 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 
+import com.android.databinding.library.baseAdapters.BR;
 import com.orhanobut.logger.Logger;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import cn.edu.zstu.sunshine.BR;
 import cn.edu.zstu.sunshine.R;
 import cn.edu.zstu.sunshine.base.BaseActivity;
 import cn.edu.zstu.sunshine.base.BaseAdapter;
 import cn.edu.zstu.sunshine.databinding.ActivityTimetableAddBinding;
 import cn.edu.zstu.sunshine.databinding.ItemOrdinalBinding;
-import cn.edu.zstu.sunshine.entity.Ordinal;
 
 public class TimetableAddActivity extends BaseActivity {
 
@@ -52,27 +48,31 @@ public class TimetableAddActivity extends BaseActivity {
                     @Override
                     public void onItemHandler(ViewDataBinding viewDataBinding, final int position) {
                         final ItemOrdinalBinding itemBinding = (ItemOrdinalBinding) viewDataBinding;
-                        if (viewModel.getWeeks().get(position).isSelected()) {
+                        if (position == weekPosition) {
+                            Logger.e("被选中的position" + position);
                             itemBinding.getRoot().setSelected(true);
+                        } else {
+                            //由于RecyclerView的布局重用机制，这里必须将其他的布局设置为未选中状态
+                            itemBinding.getRoot().setSelected(false);
                         }
 
                         itemBinding.getRoot().setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
 
-                                viewModel.getWeeks().get(weekPosition).setSelected(false);
+//                                viewModel.getWeeks().get(weekPosition).setSelected(false);
 //                                adapter.notifyItemChanged(weekPosition, viewModel.getWeeks().get(weekPosition));
-                                viewModel.getWeeks().get(position).setSelected(true);
+//                                viewModel.getWeeks().get(position).setSelected(true);
 //                                adapter.notifyItemChanged(position, viewModel.getWeeks().get(position));
                                 weekPosition = position;
 
-                                List<Ordinal> newWeeks = new ArrayList<>();
-                                newWeeks.addAll(viewModel.getWeeks());
-                                viewModel.setWeeks(newWeeks);
-                                Logger.e("当前选中的position" + weekPosition);
-                                for (cn.edu.zstu.sunshine.entity.Ordinal o : viewModel.getWeeks()) {
-                                    Logger.e("星期" + o.getId() + "；选择状态：" + o.isSelected());
-                                }
+//                                List<Ordinal> newWeeks = new ArrayList<>();
+//                                newWeeks.addAll(viewModel.getWeeks());
+//                                viewModel.setWeeks(newWeeks);
+//                                Logger.e("当前选中的position" + weekPosition);
+//                                for (cn.edu.zstu.sunshine.entity.Ordinal o : viewModel.getWeeks()) {
+//                                    Logger.e("星期" + o.getId() + "；选择状态：" + o.isSelected());
+//                                }
                                 adapter.notifyDataSetChanged();
 
 
