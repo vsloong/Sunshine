9e228f6398d7118637013360c708421f06cabb66
CooLoongWu <742420210@qq.com>
2017-11-16 17:26:20 +0800

    傻了，画出来了。。。。。

--- app/src/main/java/cn/edu/zstu/sunshine/views/RotarySeekBar.java
+++ app/src/main/java/cn/edu/zstu/sunshine/views/RotarySeekBar.java
@@ -11,8 +11,6 @@ import android.view.View;
 
 import com.orhanobut.logger.Logger;
 
-import cn.edu.zstu.sunshine.utils.UnitUtil;
-
 /**
  * 旋转式拖动条（单指针和双指针模式）
  * Created by CooLoongWu on 2017-11-13 14:28.
@@ -88,31 +86,10 @@ public class RotarySeekBar extends View {
         canvas.drawCircle(centerX, centerY, progressRadius - 110, backCirclePaint);
         canvas.drawCircle(centerX, centerY, progressRadius - 120, mainCirclePaint);
 
-        drawDay(270, 7, canvas);
-        //drawOrdinal(270, 12, canvas);
-        //drawWeek(270, 20, canvas);
+        drawDay(180, 7, canvas);
+        drawOrdinal(190, 12, canvas);
+        drawWeek(200, 20, canvas);
 
-        float radius = progressRadius - 20;
-
-        rectF.left = centerX - radius;
-        rectF.top = centerY + radius;
-        rectF.right = centerX + radius;
-        rectF.bottom = centerY - radius;
-
-//        rectF.left = UnitUtil.dp2px(centerX - radius, this.getContext());
-//        rectF.top = UnitUtil.dp2px(centerY + radius, this.getContext());
-//        rectF.right = UnitUtil.dp2px(centerX + radius, this.getContext());
-//        rectF.bottom = UnitUtil.dp2px(centerY - radius, this.getContext());
-
-        Logger.e("left:" + rectF.left + ";top:" + rectF.top + ";right:" + rectF.right + ";bottom:" + rectF.bottom);
-
-        indicatorPaint.setStrokeWidth(5);
-        canvas.drawPoint(rectF.left, rectF.top, indicatorPaint);
-        canvas.drawPoint(rectF.left, rectF.bottom, indicatorPaint);
-        canvas.drawPoint(rectF.right, rectF.top, indicatorPaint);
-        canvas.drawPoint(rectF.right, rectF.bottom, indicatorPaint);
-        canvas.drawRect(rectF, indicatorPaint);
-        canvas.drawArc(rectF, 0, 120, false, indicatorPaint);
     }
 
 
@@ -163,17 +140,12 @@ public class RotarySeekBar extends View {
 
         //课程节数刻度盘的半径
         float radius = progressRadius - 20;
+        rectF.left = centerX - radius;
+        rectF.top = centerY - radius;
+        rectF.right = centerX + radius;
+        rectF.bottom = centerY + radius;
 
-        RectF rectF = new RectF(
-                UnitUtil.dp2px(centerX - radius, this.getContext()),
-                UnitUtil.dp2px(centerY + radius, this.getContext()),
-                UnitUtil.dp2px(centerX + radius, this.getContext()),
-                UnitUtil.dp2px(centerY - radius, this.getContext())
-        );
-
-        Logger.e("left:" + rectF.left + ";top:" + rectF.top + ";right:" + rectF.right + ";bottom:" + rectF.bottom);
-
-        canvas.drawArc(rectF, 0, 120, false, indicatorPaint);
+        canvas.drawArc(rectF, 180, sweepAngle, false, indicatorPaint);
     }
 
     /**
@@ -184,27 +156,15 @@ public class RotarySeekBar extends View {
      * @param canvas     画布
      */
     private void drawWeek(int sweepAngle, int dialCount, Canvas canvas) {
-        /*
-         * 假设一个圆的圆心坐标是(a,b)，半径为r，则圆上每个点的
-         * X坐标=a + Math.sin(2*Math.PI / 360) * r ；
-         * Y坐标=b + Math.cos(2*Math.PI / 360) * r ；
-         */
+        //课程节数刻度盘的半径
+        float radius = progressRadius;
+        rectF.left = centerX - radius;
+        rectF.top = centerY - radius;
+        rectF.right = centerX + radius;
+        rectF.bottom = centerY + radius;
 
-        //每个刻度的圆心X
-        float x;
-        //每个刻度的圆心Y
-        float y;
-        //刻度间的角度偏移量（注意-1，因为10个刻度间有9段距离）
-        float angleOffset = sweepAngle / (dialCount - 1);
+        canvas.drawArc(rectF, 180, sweepAngle, false, indicatorPaint);
 
-        //角度是参考竖直方向最下端，逆时针方向
-        for (int i = 0; i < dialCount; i++) {
-            float angle = angleOffset * i + (360 - sweepAngle) / 2;
-            x = centerX + (float) (progressRadius * Math.sin(2 * Math.PI / 360 * angle));
-            y = centerY + (float) (progressRadius * Math.cos(2 * Math.PI / 360 * angle));
-
-            canvas.drawCircle(x, y, 8, indicatorPaint);
-        }
 
     }
 }
