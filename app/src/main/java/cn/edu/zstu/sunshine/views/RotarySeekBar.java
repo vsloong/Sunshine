175875fcc609f8f00ebe104425fa3b27c3484a87
CooLoongWu <742420210@qq.com>
2017-11-17 16:05:14 +0800

    添加刻度（注意精度问题）

--- app/src/main/java/cn/edu/zstu/sunshine/views/RotarySeekBar.java
+++ app/src/main/java/cn/edu/zstu/sunshine/views/RotarySeekBar.java
@@ -39,7 +39,7 @@ public class RotarySeekBar extends View {
 
     RectF rectF = new RectF();
 
-    private float progressRadius = 160;
+    private float progressRadius = 180;
 
     public RotarySeekBar(Context context) {
         super(context);
@@ -69,7 +69,7 @@ public class RotarySeekBar extends View {
 
         indicatorPaint = new Paint();
         indicatorPaint.setAntiAlias(true);
-        indicatorPaint.setStrokeWidth(1);
+        indicatorPaint.setStrokeWidth(2);
         indicatorPaint.setStyle(Paint.Style.STROKE);
         indicatorPaint.setColor(indicatorColor);
     }
@@ -83,12 +83,12 @@ public class RotarySeekBar extends View {
 
         Logger.e("当前圆心X：" + centerX + "；Y：" + centerY);
 
-        canvas.drawCircle(centerX, centerY, progressRadius - 110, backCirclePaint);
-        canvas.drawCircle(centerX, centerY, progressRadius - 120, mainCirclePaint);
+        canvas.drawCircle(centerX, centerY, progressRadius - 100, backCirclePaint);
+        canvas.drawCircle(centerX, centerY, progressRadius - 110, mainCirclePaint);
 
         drawDay(180, 7, canvas);
         drawOrdinal(190, 12, canvas);
-        drawWeek(200, 20, canvas);
+        drawWeek(200, 12, canvas);
 
     }
 
@@ -115,7 +115,7 @@ public class RotarySeekBar extends View {
         float angleOffset = sweepAngle / (dialCount - 1);
 
         //星期刻度盘的半径
-        float radius = progressRadius - 40;
+        float radius = progressRadius - 70;
 
         //角度是参考竖直方向最下端，逆时针方向
         for (int i = 0; i < dialCount; i++) {
@@ -137,15 +137,7 @@ public class RotarySeekBar extends View {
      * @param canvas     画布
      */
     private void drawOrdinal(int sweepAngle, int dialCount, Canvas canvas) {
-
-        //课程节数刻度盘的半径
-        float radius = progressRadius - 20;
-        rectF.left = centerX - radius;
-        rectF.top = centerY - radius;
-        rectF.right = centerX + radius;
-        rectF.bottom = centerY + radius;
-
-        canvas.drawArc(rectF, 180, sweepAngle, false, indicatorPaint);
+        drawDial(progressRadius - 30, sweepAngle, dialCount, canvas);
     }
 
     /**
@@ -156,15 +148,40 @@ public class RotarySeekBar extends View {
      * @param canvas     画布
      */
     private void drawWeek(int sweepAngle, int dialCount, Canvas canvas) {
+        drawDial(progressRadius, sweepAngle, dialCount, canvas);
+    }
+
+    private void drawDial(float radius, int sweepAngle, int dialCount, Canvas canvas) {
         //课程节数刻度盘的半径
-        float radius = progressRadius;
         rectF.left = centerX - radius;
         rectF.top = centerY - radius;
         rectF.right = centerX + radius;
         rectF.bottom = centerY + radius;
+        //startAngle：从横向最右侧起始点顺时针方向度量的角度
+        //sweepAngle：顺时针方向度量的角度
+        canvas.drawArc(rectF, 90 + (360 - sweepAngle) / 2, sweepAngle, false, indicatorPaint);
 
-        canvas.drawArc(rectF, 180, sweepAngle, false, indicatorPaint);
+        //绘制刻度线
+        float length = 8;//刻度线的长度
+        float startX, startY, endX, endY;
+        //刻度间的角度偏移量（必须强制转换为float，否则精度丢失！！）
+        float angleOffset = (float) sweepAngle / (dialCount - 1);
+        //角度是参考竖直方向最下端，逆时针方向（5个刻度需要有6个刻度线）
+        for (int i = 0; i < dialCount; i++) {
+//            if (i % 3 == 0 || i == 0) {
+//                indicatorPaint.setStrokeWidth(2);
+//                length = 12;
+//            } else {
+//                indicatorPaint.setStrokeWidth(1);
+//                length = 8;
+//            }
+            float angle = angleOffset * i + (360 - sweepAngle) / 2;
+            startX = centerX + (float) (radius * Math.sin(2 * Math.PI / 360 * angle));
+            startY = centerY + (float) (radius * Math.cos(2 * Math.PI / 360 * angle));
+            endX = centerX + (float) ((radius - length) * Math.sin(2 * Math.PI / 360 * angle));
+            endY = centerY + (float) ((radius - length) * Math.cos(2 * Math.PI / 360 * angle));
 
-
+            canvas.drawLine(startX, startY, endX, endY, indicatorPaint);
+        }
     }
 }
