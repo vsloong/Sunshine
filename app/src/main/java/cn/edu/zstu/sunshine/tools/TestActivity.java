f5d21ba635506f864a78b0c38292d0a278e2129e
CooLoongWu <742420210@qq.com>
2018-01-10 17:10:49 +0800

    没啥，就删了点没用的代码

--- app/src/main/java/cn/edu/zstu/sunshine/tools/TestActivity.java
+++ app/src/main/java/cn/edu/zstu/sunshine/tools/TestActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 
-import java.io.IOException;
 import java.io.OutputStream;
 
 import cn.edu.zstu.sunshine.R;
@@ -25,41 +24,5 @@ public class TestActivity extends BaseActivity {
         Log.e("编码：", str);
         str = new String(Base64.decode(str, Base64.DEFAULT));
         Log.e("解码：", str);
-
-//        MorseUtil.init();
-//        MorseUtil.show("SOS");
-
-//        simulateKey(KeyEvent.KEYCODE_BACK);
     }
-
-    /**
-     * 执行shell命令
-     *
-     * @param cmd 命令
-     */
-    private void exec(String cmd) {
-        if (os == null) {
-            try {
-                os = Runtime.getRuntime().exec("su").getOutputStream();
-                os.write(cmd.getBytes());
-                os.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public final void simulateKey(int keyCode) {
-        exec("input keyevent " + keyCode + "\n");
-    }
-
-    /**
-     * adb shell getevent -p | grep -e "0035" -e "0036" 获取屏幕宽高
-     *  我的是魅蓝5，分辨率1280*720 像素密度282，比例正好是1:1的
-     * 0035  : value 0, min 0, max 720, fuzz 0, flat 0, resolution 0
-     * 0036  : value 0, min 0, max 1280, fuzz 0, flat 0, resolution 0
-     *
-     *
-     * adb shell getevent | grep -e "0035" -e "0036" 获取屏幕点击位置坐标
-     */
 }
